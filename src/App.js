437b7b2d36b601476919e4e388177e2d0bbbc735
Carlos <elmunoz42@gmail.com>
2021-08-14 13:08:28 -0700

    adds three main components for the home page.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header() {
+  return (
+    <header>
+      <h1>Sourcerize</h1>
+
+    </header>
+  );
+}
+function Main() {
+  return (
+    <section>
+      <h3>Internet sourcery that helps your activist group source important material needs to create safety and wellness for sentient beings thus supporting a deeper connection to source.</h3>
+    </section>
+  );
+}
+function Footer(props) {
+  return (
+    <footer>
+      <p>
+        Open source since {props.year}
+      </p>
+
+    </footer>
+  );
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Main />
+      <Footer year="2021"/>
     </div>
   );
 }
