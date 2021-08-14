2b88654acffb15fd9c1461ca1ddbaf4c36308e7a
Carlos <elmunoz42@gmail.com>
2021-08-14 15:03:40 -0700

    add table with sourserings item display.

--- src/App.js
+++ src/App.js
@@ -4,16 +4,61 @@ import './App.css';
 function Header() {
   return (
     <header>
-      <h1>Sourcerize</h1>
+      <h1>sourcerise</h1>
 
     </header>
   );
 }
-function Main() {
+
+const sourcerings = [{
+    "id": 1,
+    "amount": 15,
+    "unit": "lbs",
+    "type": "food",
+    "name": "canned beans",
+    "description": "any kind"
+  },
+  {
+    "id" : 2,
+    "amount": 3,
+    "unit": "units",
+    "type": "appliance",
+    "name": "generator",
+    "description": "1500 Watts or more, gasoline, diesel or solar."
+  }
+]
+console.log(sourcerings);
+function Main(props) {
   return (
     <section>
       <h3>Internet sourcery that helps your activist group source important material needs to create safety and wellness for sentient beings thus supporting a deeper connection to source.</h3>
+      <br></br>
+      <h4>Things to source...</h4>
+      <table className="sourcerings">
+        <thead>
+          <tr>
+            <td>Amount:</td>
+            <td>Unit:</td>
+            <td>Name:</td>
+            <td>Type:</td>
+            <td>Description:</td>
+          </tr>
+        </thead >
+        {console.log(props.sourcerings.item)}
+        <tbody>
+            {Object.keys(props.sourcerings).map( (key) => {
+              console.log(props.sourcerings[key]);
+              return <tr><td>{props.sourcerings[key].amount}</td>
+                <td>{props.sourcerings[key].unit}</td>
+                <td>{props.sourcerings[key].name}</td>
+                <td>{props.sourcerings[key].type}</td>
+                <td>{props.sourcerings[key].description}</td>
+             </tr> 
+          })}
+        </tbody>
+      </table>
     </section>
+    
   );
 }
 function Footer(props) {
@@ -30,7 +75,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      <Main />
+      <Main sourcerings={sourcerings} />
       <Footer year="2021"/>
     </div>
   );
